ddc0ec115b92827082f8ec3caabc7a84e10a8ede
neerajaupasani <44835203+neerajaupasani@users.noreply.github.com>
2019-08-25 23:35:44 +0200

    Add files via upload

---
+++ cora_classification.py
@@ -0,0 +1,240 @@
+# import statements
+
+import numpy as np
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+import pandas as pd
+import networkx as nx
+
+
+# defining class with OneVsRestClassifier
+
+class TopKRanker(OneVsRestClassifier):
+    def predict(self, X, top_k_list):
+        probs = np.asarray(super(TopKRanker, self).predict_proba(X))
+        all_labels = []
+        for i, k in enumerate(top_k_list):
+            probs_ = probs[i, :]
+            labels = self.classes_[probs_.argsort()[-k:]].tolist()
+            probs_[:] = 0
+            probs_[labels] = 1
+            all_labels.append(probs_)
+        return np.asarray(all_labels)
+
+# defining the classification class
+
+class Cora_classify():
+
+    # defining the class constructor
+
+    def __init__(self, embeddings, clf):
+        self.embeddings = embeddings
+        self.clf = TopKRanker(clf)
+        self.binarizer = MultiLabelBinarizer(sparse_output=True)
+
+
+
+
+
+    def train(self, X, Y, Y_all):
+        self.binarizer.fit(Y_all)
+        X_train = [self.embeddings[x] for x in X]
+        Y = self.binarizer.transform(Y)
+        self.clf.fit(X_train, Y)
+
+    def evaluate(self, X, Y):
+        top_k_list = [len(l) for l in Y]
+        Y_ = self.predict(X, top_k_list)
+        Y = self.binarizer.transform(Y)
+
+        averages = ["micro", "macro", "samples", "weighted"]
+        results = {}
+        for average in averages:
+            results[average] = f1_score(Y, Y_, average=average)
+
+        results['acc'] = accuracy_score(Y, Y_)
+        print('-------------------')
+        print(results)
+        return results
+
+    def predict(self, X, top_k_list):
+        X_ = np.asarray([self.embeddings[x] for x in X])
+        Y = self.clf.predict(X_, top_k_list=top_k_list)
+        return Y
+
+    def split_train_evaluate(self, X, Y, train_precent,seed=0):
+        state = np.random.get_state()
+
+        training_size = int(train_precent * len(X))
+        np.random.seed(seed)
+        shuffle_indices = np.random.permutation(np.arange(len(X)))
+        X_train = [X[shuffle_indices[i]] for i in range(training_size)]
+        Y_train = [Y[shuffle_indices[i]] for i in range(training_size)]
+        X_test = [X[shuffle_indices[i]] for i in range(training_size, len(X))]
+        Y_test = [Y[shuffle_indices[i]] for i in range(training_size, len(X))]
+
+        self.train(X_train, Y_train, Y)
+        np.random.set_state(state)
+        return self.evaluate(X_test, Y_test)
+
+# loading the graph again to fill the embedding matrix from node 0 to N
+
+graph = "cora-edgelist.txt"
+type_of_graph = nx.Graph()
+
+# to read from the file
+
+new_graph = nx.read_edgelist(graph, create_using=type_of_graph, nodetype=int)
+
+
+# Loading the respective embeddings(cosine, jaccard, lhn1, pmi, sorensen, hpi, and hdi)
+
+model = pd.read_csv("cora_cembed.csv", header=None)
+model1 = pd.read_csv("cora_jembed.csv", header=None)
+model2 = pd.read_csv("cora_lembed.csv", header=None)
+model3 = pd.read_csv("cora_pembed.csv", header=None)
+model4 = pd.read_csv("cora_sembed.csv", header=None)
+model5 = pd.read_csv("cora_hembed.csv", header=None)
+model6 = pd.read_csv("cora_dembed.csv", header=None)
+
+# converting the embeddings dataframes to numpy nd arrays
+
+model_arr = model.values
+model_arr1 = model1.values
+model_arr2 = model2.values
+model_arr3 = model3.values
+model_arr4 = model4.values
+model_arr5 = model5.values
+model_arr6 = model6.values
+
+
+
+
+
+
+#building the embedding matrix from the cosine-embedding matrix from node 0 to N.
+
+embeddings = {}
+
+no = new_graph.order()
+
+for i in range(no):
+
+    embeddings[i] = model_arr[i]
+
+
+#building the embedding matrix from the jaccard-embedding matrix from node 0 to N.
+
+
+
+embeddings1 = {}
+
+no1 = new_graph.order()
+
+for i in range(no1):
+    embeddings1[i] = model_arr1[i]
+
+
+#building the embedding matrix from the lhn1-embedding matrix from node 0 to N.
+
+
+
+
+embeddings2 = {}
+
+no2 = new_graph.order()
+
+for i in range(no2):
+    embeddings2[i] = model_arr2[i]
+
+#building the embedding matrix from the pmi-embedding matrix from node 0 to N.
+
+
+embeddings3 = {}
+
+no3 = new_graph.order()
+
+
+for i in range(no3):
+    embeddings3[i] = model_arr3[i]
+
+#building the embedding matrix from the sorensen-embedding matrix from node 0 to N.
+
+
+embeddings4 = {}
+
+no4 = new_graph.order()
+
+
+for i in range(no4):
+    embeddings4[i] = model_arr4[i]
+
+#building the embedding matrix from the hpi-embedding matrix from node 0 to N.
+
+
+embeddings5 = {}
+
+no5 = new_graph.order()
+
+
+for i in range(no5):
+    embeddings5[i] = model_arr5[i]
+
+#building the embedding matrix from the hdi-embedding matrix from node 0 to N.
+
+
+embeddings6 = {}
+
+no6 = new_graph.order()
+
+
+for i in range(no6):
+    embeddings6[i] = model_arr6[i]
+
+
+
+
+
+# reading the node label.
+
+
+def read_cora_node_label(filename=None,embeddings=None):
+    fin = open(filename, 'r')
+    X = []
+    Y = []
+
+    label = {}
+
+    for line in fin:
+        a = line.strip('\n').split(' ')
+        label[a[0]] = a[1]
+
+
+    fin.close()
+    for i in embeddings:
+        X.append(i)
+        Y.append(label[str(i)])
+
+
+
+    return X, Y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ cora_evaluation.py
@@ -0,0 +1,297 @@
+#import statements
+
+import cora_classification
+from cora_classification import embeddings,embeddings1,embeddings2, embeddings3, embeddings4, embeddings5, embeddings6
+import cora_lp
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import warnings
+
+
+
+
+
+class Eval:
+
+
+    # node classification for evaluating the embeddings generated from the cosine matrix.
+
+    def cosine_classification(self,embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path,embeddings)
+
+        f_c=open('cora_nc_cos_%s.txt' % threshold, 'w')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+               print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+               clf = cora_classification.Cora_classify(embeddings=embeddings,clf=LogisticRegression())
+               results= clf.split_train_evaluate(X, Y, tr_frac)
+               warnings.filterwarnings('ignore')
+
+               for avg in [ "macro"]:
+                 f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac)+ ' '+avg+ ' '+ str('%0.5f'%results[avg]))
+                 f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the jaccard matrix.
+
+    def jaccard_classification(self,embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path,embeddings)
+
+        f_c=open('cora_nc_jac_%s.txt' % threshold, 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+               print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+               clf = cora_classification.Cora_classify(embeddings=embeddings,clf=LogisticRegression())
+               results= clf.split_train_evaluate(X, Y, tr_frac)
+               warnings.filterwarnings('ignore')
+
+               for avg in [ "macro"]:
+                 f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac)+ ' '+avg+ ' '+ str('%0.5f'%results[avg]))
+                 f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the lhn1 matrix.
+
+    def lhn1_classification(self,embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path,embeddings)
+
+        f_c=open('cora_nc_lhn1_%s.txt' % threshold, 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+               print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+               clf = cora_classification.Cora_classify(embeddings=embeddings,clf=LogisticRegression())
+               results= clf.split_train_evaluate(X, Y, tr_frac)
+               warnings.filterwarnings('ignore')
+
+               for avg in [ "macro"]:
+                 f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac)+ ' '+avg+ ' '+ str('%0.5f'%results[avg]))
+                 f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the sorensen matrix.
+
+    def sorensen_classification(self, embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path, embeddings)
+
+        f_c = open('cora_nc_sorensen_%s.txt' % threshold, 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+            print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+            clf = cora_classification.Cora_classify(embeddings=embeddings, clf=LogisticRegression())
+            results = clf.split_train_evaluate(X, Y, tr_frac)
+            warnings.filterwarnings('ignore')
+
+            for avg in ["macro"]:
+                f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac) + ' ' + avg + ' ' + str('%0.5f' % results[avg]))
+                f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the hpi matrix.
+
+    def hpi_classification(self, embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path, embeddings)
+
+        f_c = open('cora_nc_hpi_%s.txt' % threshold, 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+            print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+            clf = cora_classification.Cora_classify(embeddings=embeddings, clf=LogisticRegression())
+            results = clf.split_train_evaluate(X, Y, tr_frac)
+            warnings.filterwarnings('ignore')
+
+            for avg in ["macro"]:
+                f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac) + ' ' + avg + ' ' + str('%0.5f' % results[avg]))
+                f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the hdi matrix.
+
+    def hdi_classification(self, embeddings, label_path, name, similarity, threshold):
+
+        X, Y = cora_classification.read_cora_node_label(label_path, embeddings)
+
+        f_c = open('cora_nc_hdi_%s.txt' % threshold, 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+            print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+            clf = cora_classification.Cora_classify(embeddings=embeddings, clf=LogisticRegression())
+            results = clf.split_train_evaluate(X, Y, tr_frac)
+            warnings.filterwarnings('ignore')
+
+            for avg in ["macro"]:
+                f_c.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(tr_frac) + ' ' + avg + ' ' + str('%0.5f' % results[avg]))
+                f_c.write('\n')
+
+    # node classification for evaluating the embeddings generated from the pmi matrix.
+
+    def pmi_classification(self,embeddings, label_path, name):
+
+        X, Y = cora_classification.read_cora_node_label(label_path,embeddings)
+
+        f_c=open('cora_nc_pmi.txt', 'w+')
+
+        for tr_frac in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+
+               print("Training classifier using {:.2f}% nodes...".format(tr_frac * 100))
+               clf = cora_classification.Cora_classify(embeddings=embeddings,clf=LogisticRegression())
+               results= clf.split_train_evaluate(X, Y, tr_frac)
+               warnings.filterwarnings('ignore')
+
+               for avg in [ "macro"]:
+                 f_c.write(name + ' ' + str(tr_frac)+ ' '+avg+ ' '+ str('%0.5f'%results[avg]))
+                 f_c.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the cosine matrix.
+
+    def link_prediction_cosine(self, edge_file, embeddings, size, name, similarity, threshold):
+
+            clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file= edge_file)
+            auc=clf.predict(size)
+
+            functions = ["hadamard","average","l1","l2"]
+
+            f_l = open('cora_lp_cosine_%s.txt' % threshold, 'w')
+
+            for i in functions:
+                print(i, '%.3f' % np.mean(auc[i]))
+
+                f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+                f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the jaccard matrix.
+
+    def link_prediction_jaccard(self, edge_file, embeddings, size, name, similarity, threshold):
+
+            clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file= edge_file)
+            auc=clf.predict(size)
+
+            functions = ["hadamard","average","l1","l2"]
+
+            f_l = open('cora_lp_jaccard_%s.txt' % threshold, 'w+')
+
+            for i in functions:
+                print(i, '%.3f' % np.mean(auc[i]))
+
+                f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+                f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the lhn1 matrix.
+
+    def link_prediction_lhn1(self, edge_file, embeddings, size, name, similarity, threshold):
+
+            clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file= edge_file)
+            auc=clf.predict(size)
+
+            functions = ["hadamard","average","l1","l2"]
+
+            f_l = open('cora_lp_lhn1_%s.txt' % threshold, 'w+')
+
+            for i in functions:
+                print(i, '%.3f' % np.mean(auc[i]))
+
+                f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+                f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the sorensen matrix.
+
+    def link_prediction_sorensen(self, edge_file, embeddings, size, name, similarity, threshold):
+
+        clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file=edge_file)
+        auc = clf.predict(size)
+
+        functions = ["hadamard", "average", "l1", "l2"]
+
+        f_l = open('cora_lp_sorensen_%s.txt' % threshold, 'w+')
+
+        for i in functions:
+            print(i, '%.3f' % np.mean(auc[i]))
+
+            f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+            f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the hpi matrix.
+
+    def link_prediction_hpi(self, edge_file, embeddings, size, name, similarity, threshold):
+
+        clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file=edge_file)
+        auc = clf.predict(size)
+
+        functions = ["hadamard", "average", "l1", "l2"]
+
+        f_l = open('cora_lp_hpi_%s.txt' % threshold, 'w+')
+
+        for i in functions:
+            print(i, '%.3f' % np.mean(auc[i]))
+
+            f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+            f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the hdi matrix.
+
+    def link_prediction_hdi(self, edge_file, embeddings, size, name, similarity, threshold):
+
+        clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file=edge_file)
+        auc = clf.predict(size)
+
+        functions = ["hadamard", "average", "l1", "l2"]
+
+        f_l = open('cora_lp_hdi_%s.txt' % threshold, 'w+')
+
+        for i in functions:
+            print(i, '%.3f' % np.mean(auc[i]))
+
+            f_l.write(name + ' ' + similarity + ' ' + threshold + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+            f_l.write('\n')
+
+    # link prediction for evaluating the embeddings generated from the pmi matrix.
+
+    def link_prediction_pmi(self, edge_file, embeddings, size, name):
+
+            clf = cora_lp.Cora_linkprediction(embeddings=embeddings, edge_file= edge_file)
+            auc=clf.predict(size)
+
+            functions = ["hadamard","average","l1","l2"]
+
+            f_l = open('cora_lp_pmi_%d.txt' % size, 'w+')
+
+            for i in functions:
+                print(i, '%.3f' % np.mean(auc[i]))
+
+                f_l.write(name + ' ' + str(size) + ' ' + str(i) + ' ' + str('%.3f' % np.mean(auc[i])))
+                f_l.write('\n')
+
+
+
+
+
+eval = Eval()
+eval.cosine_classification(embeddings=embeddings,label_path='cora-label.txt',name='Cora', similarity='cosine', threshold='alpha 0.8')
+eval.jaccard_classification(embeddings=embeddings1,label_path='cora-label.txt',name='Cora', similarity='jaccard', threshold='alpha 0.8')
+eval.lhn1_classification(embeddings=embeddings2,label_path='cora-label.txt',name='Cora', similarity='lhn1', threshold='alpha 0.8')
+eval.sorensen_classification(embeddings=embeddings4,label_path='cora-label.txt',name='Cora', similarity='sorensen', threshold='alpha 0.8')
+eval.hpi_classification(embeddings=embeddings5,label_path='cora-label.txt',name='Cora', similarity='hpi', threshold='alpha 0.8')
+eval.hdi_classification(embeddings=embeddings6,label_path='cora-label.txt',name='Cora', similarity='hdi', threshold='alpha 0.8')
+
+#eval.pmi_classification(embeddings=embeddings3,label_path='cora-label.txt',name='Evaluating the embeddings generated from the pmi matrix')
+
+eval.link_prediction_cosine('cora-edgelist.txt',embeddings=embeddings,size=128,name='Cora', similarity='cosine', threshold='alpha 0.8')
+eval.link_prediction_jaccard('cora-edgelist.txt',embeddings=embeddings1,size=128,name='Cora', similarity='jaccard', threshold='alpha 0.8')
+eval.link_prediction_lhn1('cora-edgelist.txt',embeddings=embeddings2,size=128,name='Cora', similarity='lhn1', threshold='alpha 0.8')
+eval.link_prediction_sorensen('cora-edgelist.txt',embeddings=embeddings4,size=128,name='Cora', similarity='sorensen', threshold='alpha 0.8')
+eval.link_prediction_hpi('cora-edgelist.txt',embeddings=embeddings5,size=128,name='Cora', similarity='hpi', threshold='alpha 0.8')
+eval.link_prediction_hdi('cora-edgelist.txt',embeddings=embeddings6,size=128,name='Cora', similarity='hdi', threshold='alpha 0.8')
+#eval.link_prediction_pmi('cora-edgelist.txt',embeddings=embeddings3,size=128,name='Evaluating the embeddings generated from the pmi matrix')
+
+
+
+
+
+
+
+
